1345a489743770ddb46af8dd23273b96ab758768
Eugene Lukianov <eugene.lukianov@gmail.com>
2021-02-22 16:36:34 +0300

    CustomerIdGenerator

---
+++ order/persistence/src/test/kotlin/com/stringconcat/ddd/order/persistence/order/InMemoryIncrementalCustomerOrderIdGeneratorTest.kt
@@ -0,0 +1,15 @@
+package com.stringconcat.ddd.order.persistence.order
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+internal class InMemoryIncrementalCustomerOrderIdGeneratorTest {
+
+    @Test
+    fun `id is incremented`() {
+        val generator = InMemoryIncrementalCustomerOrderIdGenerator()
+        val orderId1 = generator.generate()
+        val orderId2 = generator.generate()
+        orderId1.value shouldBe orderId2.value - 1
+    }
+}
