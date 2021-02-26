6ca467ea2260dfc63555d51939fb0d4120ae2215
Eugene Lukianov <eugene.lukianov@gmail.com>
2021-02-26 23:22:33 +0300

    GetMenuCommandTest

--- application/src/main/kotlin/com/stringconcat/dev/course/app/telnet/order/menu/GetMenuCommand.kt
+++ application/src/main/kotlin/com/stringconcat/dev/course/app/telnet/order/menu/GetMenuCommand.kt
@@ -2,17 +2,15 @@ package com.stringconcat.dev.course.app.telnet.order.menu
 
 import com.github.freva.asciitable.AsciiTable
 import com.github.freva.asciitable.Column
-import com.khubla.telnet.nvt.NVT
-import com.khubla.telnet.shell.command.TelnetCommand
 import com.stringconcat.ddd.order.usecase.menu.GetMenu
-import java.util.HashMap
+import com.stringconcat.dev.course.app.telnet.ApplicationTelnetCommand
 
-class GetMenuCommand(private val useCase: GetMenu) : TelnetCommand {
+class GetMenuCommand(private val useCase: GetMenu) : ApplicationTelnetCommand() {
 
-    override fun execute(nvt: NVT, line: String, sessionParameters: HashMap<String, Any>): Boolean {
+    override fun execute(line: String, sessionParameters: Map<String, Any>): String {
         val menu = useCase.execute()
 
-        val table = AsciiTable.getTable(
+        return AsciiTable.getTable(
             AsciiTable.FANCY_ASCII, menu, listOf(
                 Column().with { meal -> meal.id.toString() },
                 Column().header("Name").with { meal -> meal.name },
@@ -20,8 +18,6 @@ class GetMenuCommand(private val useCase: GetMenu) : TelnetCommand {
                 Column().header("Price").with { meal -> meal.price.toPlainString() },
             )
         )
-        nvt.writeln(table.replace("\n", "\r\n"))
-        return true
     }
 
     override fun getNames() = arrayOf("menu")
--- application/src/test/kotlin/com/stringconcat/dev/course/app/telnet/order/menu/GetMenuCommandTest.kt
+++ application/src/test/kotlin/com/stringconcat/dev/course/app/telnet/order/menu/GetMenuCommandTest.kt
@@ -1,9 +1,43 @@
 package com.stringconcat.dev.course.app.telnet.order.menu
 
+import com.stringconcat.ddd.order.usecase.menu.GetMenu
+import com.stringconcat.ddd.order.usecase.menu.MealInfo
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class GetMenuCommandTest {
 
     @Test
-    fun `get cart - cart is empty`() = Unit
+    fun `get cart - cart is empty`() {
+
+        val command = GetMenuCommand(EmptyUseCase)
+        val result = command.execute("", emptyMap())
+        result shouldBe
+                "╔══╤══════╤═════════════╤═══════╗\n" +
+                "║  │ Name │ Description │ Price ║\n" +
+                "╠══╪══════╪═════════════╪═══════╣\n" +
+                "╚══╧══════╧═════════════╧═══════╝"
+    }
+
+    @Test
+    fun `get cart - cart is not empty`() {
+        val command = GetMenuCommand(NotEmptyUseCase)
+        val result = command.execute("", emptyMap())
+        result shouldBe
+                "╔═══╤═══════════╤═════════════╤═══════╗\n" +
+                "║   │ Name      │ Description │ Price ║\n" +
+                "╠═══╪═══════════╪═════════════╪═══════╣\n" +
+                "║ 1 │ meal name │ description │    10 ║\n" +
+                "╚═══╧═══════════╧═════════════╧═══════╝"
+    }
+
+    private object EmptyUseCase : GetMenu {
+        override fun execute() = emptyList<MealInfo>()
+    }
+
+    private object NotEmptyUseCase : GetMenu {
+        override fun execute() =
+            listOf(MealInfo(id = 1, name = "meal name", description = "description", price = BigDecimal.TEN))
+    }
 }
