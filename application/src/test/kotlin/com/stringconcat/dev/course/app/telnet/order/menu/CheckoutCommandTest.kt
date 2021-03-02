34780e066efcddb49bfbcbaede7896630d3ae9cc
Sergey <sergey.bukharov@thoughtworks.com>
2021-03-02 13:24:44 +0800

    fix imports

--- application/src/test/kotlin/com/stringconcat/dev/course/app/telnet/order/menu/CheckoutCommandTest.kt
+++ application/src/test/kotlin/com/stringconcat/dev/course/app/telnet/order/menu/CheckoutCommandTest.kt
@@ -13,7 +13,7 @@ import com.stringconcat.dev.course.app.price
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.shouldBe
 import java.net.URL
-import java.util.*
+import java.util.UUID
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
