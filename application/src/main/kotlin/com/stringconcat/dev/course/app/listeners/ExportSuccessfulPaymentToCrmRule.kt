2447e471207da4d9875885a9c39a3b21f1898511
Dmitrii Abramov <karvozavr@gmail.com>
2021-03-18 18:18:44 +0300

    Implement ExportSuccessfulPaymentToCrmRule using ExportPaymentData usecase

--- application/src/main/kotlin/com/stringconcat/dev/course/app/listeners/ExportSuccessfulPaymentToCrmRule.kt
+++ application/src/main/kotlin/com/stringconcat/dev/course/app/listeners/ExportSuccessfulPaymentToCrmRule.kt
@@ -1,25 +1,18 @@
 package com.stringconcat.dev.course.app.listeners
 
 import com.stringconcat.ddd.order.domain.order.CustomerOrderHasBeenPaidDomainEvent
-import com.stringconcat.ddd.order.domain.payment.OrderPayment
-import com.stringconcat.ddd.order.usecase.order.CustomerOrderExtractor
-import com.stringconcat.ddd.order.usecase.payment.PaymentExporter
+import com.stringconcat.ddd.order.usecase.payment.ExportPaymentData
 import com.stringconcat.dev.course.app.event.DomainEventListener
 
 class ExportSuccessfulPaymentToCrmRule(
-    private val paymentExporter: PaymentExporter,
-    private val orderExtractor: CustomerOrderExtractor
+    private val exportPaymentData: ExportPaymentData
 ) : DomainEventListener<CustomerOrderHasBeenPaidDomainEvent> {
 
     override fun eventType() = CustomerOrderHasBeenPaidDomainEvent::class
 
     override fun handle(event: CustomerOrderHasBeenPaidDomainEvent) {
-        val order = orderExtractor.getById(event.orderId)
-        checkNotNull(order) {
-            "Order ${event.orderId} not found"
+        exportPaymentData.execute(event.orderId).mapLeft { error ->
+            throw IllegalStateException(error.message)
         }
-
-        val orderPayment = OrderPayment(orderId = order.id, price = order.totalPrice())
-        paymentExporter.exportPayment(payment = orderPayment)
     }
 }
--- application/src/test/kotlin/com/stringconcat/dev/course/app/listeners/ExportSuccessfulPaymentToCrmRuleTest.kt
+++ application/src/test/kotlin/com/stringconcat/dev/course/app/listeners/ExportSuccessfulPaymentToCrmRuleTest.kt
@@ -1,46 +1,71 @@
 package com.stringconcat.dev.course.app.listeners
 
-import com.stringconcat.ddd.order.domain.cart.CustomerId
-import com.stringconcat.ddd.order.domain.order.CustomerOrder
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.stringconcat.ddd.order.domain.order.CustomerOrderHasBeenPaidDomainEvent
 import com.stringconcat.ddd.order.domain.order.CustomerOrderId
-import com.stringconcat.ddd.order.domain.order.OrderState
-import com.stringconcat.ddd.order.domain.payment.OrderPayment
-import com.stringconcat.ddd.order.usecase.order.CustomerOrderExtractor
-import com.stringconcat.ddd.order.usecase.payment.PaymentExporter
-import com.stringconcat.dev.course.app.count
-import com.stringconcat.dev.course.app.customerOrder
-import com.stringconcat.dev.course.app.orderItem
+import com.stringconcat.ddd.order.usecase.payment.ExportPaymentData
+import com.stringconcat.ddd.order.usecase.payment.ExportPaymentDataError
 import com.stringconcat.dev.course.app.orderId
-import com.stringconcat.dev.course.app.price
-import io.kotest.assertions.fail
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import java.math.BigDecimal
 
 internal class ExportSuccessfulPaymentToCrmRuleTest {
-/*
+
     @Test
     fun `should have correct type`() {
-        val rule = ExportSuccessfulPaymentToCrmRule(
-            paymentExporter = TestPaymentsExporter(),
-            orderExtractor = TestCustomerOrderExtractor(order = customerOrder())
-        )
-
+        val rule = ExportSuccessfulPaymentToCrmRule(TestExportPaymentData())
         rule.eventType() shouldBe CustomerOrderHasBeenPaidDomainEvent::class
     }
 
     @Test
     fun `should throw error on non-existing order type`() {
         val rule = ExportSuccessfulPaymentToCrmRule(
-            paymentExporter = TestPaymentsExporter(),
-            orderExtractor = TestCustomerOrderExtractor(order = null)
+            exportPaymentData = TestExportPaymentData(ExportPaymentDataError.OrderNotFound)
         )
 
         shouldThrow<IllegalStateException> {
             rule.handle(CustomerOrderHasBeenPaidDomainEvent(orderId = orderId()))
+        }.message shouldBe ExportPaymentDataError.OrderNotFound.message
+    }
+
+    @Test
+    fun `should throw error on invalid order state`() {
+        val rule = ExportSuccessfulPaymentToCrmRule(
+            exportPaymentData = TestExportPaymentData(ExportPaymentDataError.InvalidOrderState)
+        )
+
+        shouldThrow<IllegalStateException> {
+            rule.handle(CustomerOrderHasBeenPaidDomainEvent(orderId = orderId()))
+        }.message shouldBe ExportPaymentDataError.InvalidOrderState.message
+    }
+
+    @Test
+    fun `should successfully export data`() {
+        val exportPaymentData = TestExportPaymentData()
+        val orderId = orderId()
+
+        val rule = ExportSuccessfulPaymentToCrmRule(exportPaymentData)
+        rule.handle(CustomerOrderHasBeenPaidDomainEvent(orderId = orderId))
+
+        exportPaymentData.receivedOrderId shouldBe orderId
+    }
+
+    class TestExportPaymentData(
+        private val error: ExportPaymentDataError? = null,
+    ) : ExportPaymentData {
+
+        var receivedOrderId: CustomerOrderId? = null
+
+        override fun execute(orderId: CustomerOrderId): Either<ExportPaymentDataError, Unit> {
+            receivedOrderId = orderId
+            if (error != null) {
+                return error.left()
+            }
+
+            return Unit.right()
         }
-    }*/
+    }
 }
