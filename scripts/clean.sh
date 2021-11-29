ad2f776b9f8663adefe185aeebbd86270f90e428
Eugene <elukianov@stringconcat.com>
2021-11-29 23:15:44 +0300

    Infrastructure fixes

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,5 +3,5 @@ set -e
 currentDir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 rootDir="$currentDir/../"
 
-(cd "$rootDir" && exec ./scripts/stop.sh)
+(cd "$rootDir" && exec docker-compose down -v)
 (cd "$rootDir" && exec docker-compose rm -f)
