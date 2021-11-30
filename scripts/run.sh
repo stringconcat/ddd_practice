6d25c93505a47173e48f98ea0fa1170096345830
Eugene <elukianov@stringconcat.com>
2021-11-30 14:58:13 +0300

    run.sh enhancements

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,3 +6,7 @@ rootDir="$currentDir/../"
 (cd "$rootDir" && exec ./scripts/build.sh)
 (cd "$rootDir" && exec ./application/buildImage.sh)
 (cd "$rootDir" && exec docker-compose up -d)
+
+echo 'Portainer GUI is available at http://localhost:9000/#/dashboard'
+echo 'pgadmin is available at http://localhost:15432/ Login: restaurant@stringconcat.com:restaurant Database password is restaurant'
+python -mwebbrowser http://localhost:9000/#/dashboard || true
