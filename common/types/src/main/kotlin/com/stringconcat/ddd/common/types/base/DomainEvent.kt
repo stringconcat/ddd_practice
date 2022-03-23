8c986776ce378c0ebe1f829a6c9447b72037ff3a
Eugene <elukianov@stringconcat.com>
2022-03-23 08:51:02 +0300

    Moved created to Event from EventId

--- common/types/src/main/kotlin/com/stringconcat/ddd/common/types/base/DomainEvent.kt
+++ common/types/src/main/kotlin/com/stringconcat/ddd/common/types/base/DomainEvent.kt
@@ -5,9 +5,10 @@ import java.util.UUID
 
 open class DomainEvent protected constructor() {
     val id = EventId.generate()
+    val created = OffsetDateTime.now()
 }
 
-data class EventId internal constructor(val value: UUID, val created: OffsetDateTime = OffsetDateTime.now()) {
+data class EventId internal constructor(val value: UUID) {
     companion object {
         fun generate(): EventId = EventId(UUID.randomUUID())
     }
