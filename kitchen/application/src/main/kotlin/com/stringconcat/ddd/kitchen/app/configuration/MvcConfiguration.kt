7b0c05b68d851bbb245c504921340b04bd429933
Olga Fedorova <fedorova-olga.ifo@yandex.ru>
2022-04-02 11:39:29 +0300

    Add webjars into kitchen

--- kitchen/application/src/main/kotlin/com/stringconcat/ddd/kitchen/app/configuration/MvcConfiguration.kt
+++ kitchen/application/src/main/kotlin/com/stringconcat/ddd/kitchen/app/configuration/MvcConfiguration.kt
@@ -4,11 +4,19 @@ import com.stringconcat.ddd.common.rest.GlobalErrorHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-class MvcConfiguration {
+class MvcConfiguration : WebMvcConfigurer {
 
     @Bean
     fun globalErrorHandler() = GlobalErrorHandler()
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/")
+    }
 }
--- shop/application/src/main/kotlin/com/stringconcat/ddd/shop/app/configuration/MvcConfiguration.kt
+++ shop/application/src/main/kotlin/com/stringconcat/ddd/shop/app/configuration/MvcConfiguration.kt
@@ -4,19 +4,11 @@ import com.stringconcat.ddd.shop.app.controllers.ShopErrorHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
-class MvcConfiguration : WebMvcConfigurer {
+class MvcConfiguration {
 
     @Bean
     fun globalErrorHandler() = ShopErrorHandler()
-
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry
-            .addResourceHandler("/webjars/**")
-            .addResourceLocations("/webjars/")
-    }
 }
